cc27cf121496782c052e87ea56cbadb18acce5dc
Илья <i-hawk18@mail.ru>
2023-04-13 01:52:32 +0300

    99.999999999999999%

--- flutter_application_1/lib/our_app.dart
+++ flutter_application_1/lib/our_app.dart
@@ -10,6 +10,8 @@ import 'info_page.dart';
 class App extends StatelessWidget {
   const App({super.key});
 
+  
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
