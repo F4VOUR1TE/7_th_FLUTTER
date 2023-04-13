34ad1e0f6168f041ec514830de2e612028958a31
Andrei <turutin2002@gmail.com>
2023-04-13 20:56:33 +0300

    backend changes

--- FlaskBackend/run.py
+++ FlaskBackend/run.py
@@ -2,5 +2,6 @@ from modules import app
 # 
 # model = load_model("imagemodel.h5")
 # print("model loading .... plaese wait this might take a while")
+# flask --app run run --host=0.0.0.0
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=True, host='192.168.0.104', port=80)
